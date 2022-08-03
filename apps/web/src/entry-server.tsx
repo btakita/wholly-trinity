b5bc5ee403716d68d9f78a97bd8aedfb9dcdc29f
Brian Takita <brian.takita@gmail.com>
2022-08-03 01:45:33 -0500

    initial monorepo + solidjs setup

---
+++ apps/web/src/entry-server.tsx
@@ -0,0 +1,4 @@
+import { createHandler, renderAsync, StartServer, } from 'solid-start/entry-server'
+export default createHandler(
+	renderAsync((context)=><StartServer context={context}/>)
+)
---
+++ bin/tmux-dev.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+nvmrc=~/.nvm/nvm.sh
+if [ -e $nvmrc ]; then
+  source $nvmrc
+  nvm use
+fi
+DIR_DEFAULT=~/work/stargate/wholly-trinity/
+DIR="${DIR:-$DIR_DEFAULT}"
+
+cd $DIR
+tmux rename-window wholly-trinity
+tmux send-keys 'nvm use' C-m
+cd $DIR
+tmux split-window -v $SHELL
+tmux send-keys 'nvm use' C-m
+cd $DIR/apps/web
+tmux split-window -h $SHELL
+tmux send-keys 'nvm use' C-m
+tmux select-pane -t 0
+cd $DIR/apps/web
+tmux split-window -h $SHELL
+tmux send-keys 'nvm use' C-m
+tmux send-keys 'pnpm run dev' C-m
+tmux select-pane -t 0
+
+tmux select-window -t 0
