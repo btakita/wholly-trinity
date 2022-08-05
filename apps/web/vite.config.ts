ca1d939708b2c6475ff7698629235495c4348df9
Brian Takita <brian.takita@gmail.com>
2022-08-04 22:54:08 -0500

    @wholly-trinity/web: vite.config.ts: + adapter: netlify({ edge: true })

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,5 +1,12 @@
+import netlify from 'solid-start-netlify'
 import solid from 'solid-start/vite'
 import { defineConfig } from 'vite'
 export default defineConfig({
-	plugins: [solid()],
+	plugins: [solid({
+		...(
+			process.env.NODE_ENV === 'development'
+			? {}
+			: { adapter: netlify({ edge: true }) }
+		)
+	})],
 })
