cd57c1513c520382603c95057d33a50378de5e4e
Brian Takita <brian.takita@gmail.com>
2022-08-04 23:48:19 -0500

    + solid-start-vercel

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,13 +1,14 @@
 import cloudflare_pages from 'solid-start-cloudflare-pages'
 import netlify from 'solid-start-netlify'
+import vercel from 'solid-start-vercel'
 import solid from 'solid-start/vite'
 import { defineConfig } from 'vite'
 export default defineConfig({
 	plugins: [solid({
 		...(
-			process.env.NODE_ENV === 'development'
-			? {}
-			: { adapter: cloudflare_pages() }
+			process.env.DEPLOY
+			? { adapter: vercel() }
+			: {}
 		)
 	})],
 })
