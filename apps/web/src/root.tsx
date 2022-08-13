6e40a67f37d0bf494b249464c426fa3b7c2b820b
Brian Takita <brian.takita@gmail.com>
2022-08-12 23:03:30 -0500

    + favicon.ico: from https://web.archive.org/web/20220403121104im_/http://www.bluestargate.com/wp-content/uploads/2014/03/favicon-21.ico

--- apps/web/src/root.tsx
+++ apps/web/src/root.tsx
@@ -1,23 +1,24 @@
 // @refresh reload
 import { Suspense } from 'solid-js'
-import { Body, ErrorBoundary, FileRoutes, Head, Html, Meta, Routes, Scripts } from 'solid-start'
+import { Body, ErrorBoundary, FileRoutes, Head, Html, Link, Meta, Routes, Scripts } from 'solid-start'
 import './index.css'
 export default function Root() {
 	return (
 		<Html lang="en">
-      <Head>
-        <Meta charset="utf-8"/>
-        <Meta name="viewport" content="width=device-width, initial-scale=1"/>
-      </Head>
-      <Body class="antialiased">
-        <ErrorBoundary>
-          <Suspense>
+			<Head>
+				<Meta charset="utf-8"/>
+				<Meta name="viewport" content="width=device-width, initial-scale=1"/>
+				<Link rel="icon" type="image/png" href="/favicon.ico"/>
+			</Head>
+			<Body class="antialiased">
+				<ErrorBoundary>
+					<Suspense>
 						<Routes>
 							<FileRoutes/>
 						</Routes>
-          </Suspense>
-        </ErrorBoundary>
-        <Scripts/>
-      </Body>
-    </Html>)
+					</Suspense>
+				</ErrorBoundary>
+				<Scripts/>
+			</Body>
+		</Html>)
 }
