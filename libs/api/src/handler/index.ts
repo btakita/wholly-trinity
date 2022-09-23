a8074987c45762e5130a8aca711fc5ad4b869815
Brian Takita <brian.takita@gmail.com>
2022-09-23 11:48:51 -0400

    @wholly-trinity/api: fix: contact__set__handle

--- libs/api/src/handler/index.ts
+++ libs/api/src/handler/index.ts
@@ -2,9 +2,16 @@ import { compact } from '@ctx-core/array'
 import { be_, type Ctx, ctx_ } from '@ctx-core/object'
 import { query_str_ } from '@ctx-core/uri'
 import { VercelRequest, VercelResponse } from '@vercel/node'
-import { google__error__payload_T } from '@wholly-trinity/types'
-import type { api__cmd_T, append__payload_T, google__credentials_T, payload_T } from '@wholly-trinity/types'
-import { JWT } from 'google-auth-library'
+import type {
+	api__cmd_T,
+	append__payload_T,
+	google__credentials_T,
+	google__error__payload_T,
+	payload_T
+} from '@wholly-trinity/types'
+import jws from 'jws'
+import { createRequire } from 'module'
+const require = createRequire(import.meta.url)
 const sheets__url = `https://content-sheets.googleapis.com/v4/spreadsheets/${process.env.SHEET_ID}`
 export async function handler(req:VercelRequest, res:VercelResponse):Promise<void> {
 	const ctx = ctx_()
@@ -12,13 +19,13 @@ export async function handler(req:VercelRequest, res:VercelResponse):Promise<voi
 	res.send(JSON.stringify(payload))
 }
 export async function payload_(ctx:Ctx, req:VercelRequest|Request):Promise<payload_T> {
-	await contact__set__handle(ctx, req)
+	const req__payload = ((req as Request).json ? await (req as Request).json() : req.body) as api__cmd_T
+	await contact__set__handle(ctx, req__payload)
 	await ping__handle(ctx)
 	return payload__(ctx)
 }
-async function contact__set__handle(ctx:Ctx, req:VercelRequest|Request):Promise<payload_T> {
+export async function contact__set__handle(ctx:Ctx, req__payload:api__cmd_T):Promise<payload_T> {
 	const payload = payload__(ctx)
-	const req__payload = ((req as Request).json ? await (req as Request).json() : req.body) as api__cmd_T
 	const contact__set = req__payload.contact__set
 	if (!contact__set) return
 	const { email, phone } = contact__set
@@ -26,27 +33,7 @@ async function contact__set__handle(ctx:Ctx, req:VercelRequest|Request):Promise<
 		payload.contact__set = { status: 400, code: 'MISSING_EMAIL_PHONE', message: 'Include email or phone' }
 	}
 	try {
-		const { private_key, client_email, token_uri }:google__credentials_T = JSON.parse(process.env.GOOGLE_CREDENTIALS)
-		console.debug('contact__set__handle|debug|1')
-		const access_token = await fetch(token_uri, {
-			method: 'POST',
-			headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-			body: query_str_({
-				iss: client_email,
-				sub: null,
-				scope: 'https://www.googleapis.com/auth/spreadsheets',
-				keyFile: null,
-				key: private_key,
-			}, '')
-		}).then($=>$.text())
-		console.debug('contact__set__handle|debug|2', { access_token })
-		// const jwt = new JWT({
-		// 	email: google__credentials.client_email,
-		// 	key: google__credentials.private_key,
-		// 	scopes: 'https://www.googleapis.com/auth/spreadsheets',
-		// 	subject: null
-		// })
-		// const access_token = await jwt.authorize().then($=>$.access_token)
+		const access_token = await access_token_()
 		const append__payload:append__payload_T = await fetch(`${sheets__url}/values/A1:append${query_str_({
 			responseDateTimeRenderOption: 'SERIAL_NUMBER',
 			includeValuesInResponse: true,
@@ -61,8 +48,6 @@ async function contact__set__handle(ctx:Ctx, req:VercelRequest|Request):Promise<
 			},
 			body: JSON.stringify({ values: [[email, phone, new Date().toUTCString()]] })
 		}).then($=>$.json())
-		console.debug('debug|1')
-		console.debug(JSON.stringify(append__payload, null, 2))
 		const error__google__error__payload = append__payload as google__error__payload_T
 		if (error__google__error__payload.error) {
 			console.error(error__google__error__payload.error)
@@ -81,6 +66,39 @@ async function contact__set__handle(ctx:Ctx, req:VercelRequest|Request):Promise<
 		return payload
 	}
 	return payload
+	async function access_token_() {
+		const { private_key, client_email, token_uri }:google__credentials_T = JSON.parse(process.env.GOOGLE_CREDENTIALS)
+		const grant_type = 'urn:ietf:params:oauth:grant-type:jwt-bearer'
+		let scope = 'https://www.googleapis.com/auth/spreadsheets'
+		const aud = 'https://www.googleapis.com/oauth2/v4/token'
+		const iat = Math.floor(new Date().getTime() / 1000)
+		const assertion = jws.sign({
+			header: { alg: 'RS256' },
+			payload: {
+				iss: client_email,
+				scope,
+				aud,
+				exp: iat + 3600,
+				iat,
+				sub: null
+			},
+			secret: private_key
+		})
+		const payload = await fetch(token_uri, {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+			body: query_str_({
+				grant_type,
+				assertion,
+				iss: client_email,
+				sub: null,
+				scope: scope,
+				keyFile: null,
+				key: private_key,
+			}, '')
+		}).then($=>$.json())
+		return payload.access_token
+	}
 }
 async function ping__handle(ctx:Ctx) {
 	/** @type {payload_T} */
