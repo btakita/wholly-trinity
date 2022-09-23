ed2b4d2d6d800ff87adf9e0cc37d40ede2a73507
Brian Takita <brian.takita@gmail.com>
2022-09-23 18:34:23 -0400

    @wholly-trinity/web:
    	vercel.json in root path instead of .vercel
    
    vercel.json: + ./vercel

--- apps/web/rollup.config.mjs
+++ apps/web/rollup.config.mjs
@@ -1,11 +1,9 @@
-import { copyFileSync, existsSync, readdirSync, readFileSync } from 'fs'
-import { dirname, resolve } from 'path'
+import { readFileSync } from 'fs'
 import commonjs_ from '@rollup/plugin-commonjs'
 import node_resolve_ from '@rollup/plugin-node-resolve'
 import replace_ from '@rollup/plugin-replace'
 import esbuild_ from 'rollup-plugin-esbuild'
 import { pick } from '@ctx-core/object'
-const dir = dirname(new URL(import.meta.url).pathname)
 const compilerOptions = pick(
 	JSON.parse(readFileSync('./tsconfig.json').toString()).compilerOptions,
 	'target', 'jsx', 'jsxImportSource')
@@ -20,24 +18,4 @@ export default {
 		commonjs_(),
 		node_resolve_({ browser: true }),
 	]
-}
-function copyFiles(from, to, overwrite = false) {
-	return {
-		name: 'copy-files',
-		generateBundle() {
-			const log = msg=>console.log('\x1b[36m%s\x1b[0m', msg)
-			log(`copy files: ${from} → ${to}`)
-			readdirSync(from).forEach(file=>{
-				const fromFile = `${from}/${file}`
-				const toFile = `${to}/${file}`
-				if (existsSync(toFile) && !overwrite)
-					return
-				log(`• ${fromFile} → ${toFile}`)
-				copyFileSync(
-					resolve(fromFile),
-					resolve(toFile)
-				)
-			})
-		}
-	}
 }
