7bef60ff838715b588ab79f7253ba1165b3578f7
Brian Takita <brian.takita@gmail.com>
2023-10-07 14:26:13 -0400

    @wholly-trinity/www.stargateevent.com: + ∋ @astrojs/vercel:
    	astro.config.mjs
    	package.json

--- apps/www.stargateevent.com/astro.config.mjs
+++ apps/www.stargateevent.com/astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config'
 import solid from '@astrojs/solid-js'
 import tailwind from '@astrojs/tailwind'
+import vercel from '@astrojs/vercel/serverless'
 // https://astro.build/config
 export default defineConfig({
 	integrations: [solid(), tailwind()],
+	output: 'server',
+	adapter: vercel(),
 })
